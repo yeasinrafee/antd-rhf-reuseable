ce1cac2681b076eb0193283978154b979b79981e
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-01-25 11:52:05 +0600

    Mode added to select item

--- src/components/ui/form/AntdRHFSelect.tsx
+++ src/components/ui/form/AntdRHFSelect.tsx
@@ -4,20 +4,27 @@ import { Controller } from 'react-hook-form';
 type TAntdRHFSelectProps = {
   name: string;
   label: string;
-  options: { value: string; label: string; disabled?: boolean }[];
+  options?: { value: string; label: string; disabled?: boolean }[];
+  mode?: 'multiple' | undefined;
 };
 
 export default function AntdRHFSelect({
   name,
   label,
   options,
+  mode,
 }: TAntdRHFSelectProps) {
   return (
     <Controller
       name={name}
       render={({ field, fieldState: { error } }) => (
         <Form.Item label={label}>
-          <Select style={{ width: '100%' }} {...field} options={options} />
+          <Select
+            mode={mode}
+            style={{ width: '100%' }}
+            {...field}
+            options={options}
+          />
           {error && <small style={{ color: 'red' }}>{error.message}</small>}
         </Form.Item>
       )}
